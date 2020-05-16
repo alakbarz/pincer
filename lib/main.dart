b64f50864e192ffef4b06983da81869fb7b84480
Alakbar <me@alak.bar>
2020-05-17 00:31:09 +0100

    Fetches and parses JSON + Loading and displaying hottest and newest stories + Opening story links in browser

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,46 +39,100 @@ class MyStatefulWidget extends StatefulWidget {
   MyStatefulWidget({Key key}) : super(key: key);
 
   @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+  MyStatefulWidgetState createState() => MyStatefulWidgetState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+class MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
 
   List<Widget> _widgetOptions = <Widget>[
-    ListView.builder(
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text('News Story Number'),
-          subtitle: Text('This is a subtitle for the story number'),
-          trailing: Container(
-            child: Text('$index'),
-          ),
-          onTap: () {
-            // TODO onTap for ListView story selection
-          },
-          onLongPress: () {
-            // TODO onLongPress for ListView story selection
-          },
-        );
-      },
-    ),
-    Text('Latest'),
     ListView(
       children: <Widget>[
-        Container (
-          child: ListTile(
-            title: Text('Hello'),
-            trailing: Checkbox(onChanged: (value) {
-              // TODO Settings checkbox
-              value = true;
-            }, value: false,),
-          ),
-        )
+        new FutureBuilder<List<Newest>>(
+            future: fetchHottest(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Newest> posts = snapshot.data;
+                return new Column(
+                    children: posts
+                        .map((post) => new ListTile(
+                            onTap: () async {
+                              if (await canLaunch(post.url.toString())) {
+                                await launch(post.url.toString());
+                              }
+                            },
+                            title: Text(post.title),
+                            subtitle: Text(post.submitterUser.username +
+                                ' · ' +
+                                cutDomain(post.url) +
+                                ' · ▲' +
+                                post.upvotes.toString())))
+                        .toList());
+              } else if (snapshot.hasError) {
+                return snapshot.error;
+              }
+
+              return LinearProgressIndicator();
+            })
+      ],
+    ),
+
+    // Latest
+    ListView(
+      children: <Widget>[
+        new FutureBuilder<List<Newest>>(
+            future: fetchNewest(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Newest> posts = snapshot.data;
+                return new Column(
+                    children: posts
+                        .map((post) => new ListTile(
+                            onTap: () async {
+                              if (await canLaunch(post.url.toString())) {
+                                await launch(post.url.toString());
+                              }
+                            },
+                            title: Text(post.title),
+                            subtitle: Text(post.submitterUser.username +
+                                ' · ' +
+                                cutDomain(post.url) +
+                                ' · ▲' +
+                                post.upvotes.toString())))
+                        .toList());
+              } else if (snapshot.hasError) {
+                return snapshot.error;
+              }
+
+              return LinearProgressIndicator();
+            })
+      ],
+    ),
+
+    //Settings
+    ListView(
+      children: <Widget>[
+        CheckboxListTile(
+          title: Text('Animate Slowly'),
+          subtitle: Text('Slows down animations by a factor of 10'),
+          value: timeDilation != 1.0,
+          onChanged: (bool value) {
+            //            setState(() {
+            //              timeDilation = value ? 10.0 : 1.0;
+            //            });
+          },
+        ),
+        CheckboxListTile(
+          title: Text('Dark Mode'),
+          subtitle: Text('Woah'),
+          value: false,
+          onChanged: (bool value) {},
+        ),
       ],
     ),
   ];
 
+  // Setting the title of the AppBar depending on which page is chosen
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -96,6 +154,20 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           break;
       }
     });
+  }
+
+  // Extracts the top level domain from a URL
+  static String cutDomain(String url) {
+    RegExp exp =
+        new RegExp(r'^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)');
+
+    RegExpMatch match = exp.firstMatch(url);
+
+    if (match == null){
+      return 'Discussion Thread';
+    }
+
+    return match.group(1);
   }
 
   @override
@@ -135,10 +207,148 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   }
 }
 
-//      floatingActionButton: FloatingActionButton(
-//        onPressed: () {
-//          // onPressed code goes here
-//        },
-//        child: Icon(Icons.search),
-//        backgroundColor: themeColour,
-//      ),
+Future<List<Newest>> fetchNewest() async {
+  final response = await http.get('https://lobste.rs/newest.json');
+  var responseJson = json.decode(response.body);
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+//    return Newest.fromJson(json.decode(response.body));
+    return (responseJson as List).map((e) => Newest.fromJson(e)).toList();
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load newest stories');
+  }
+}
+
+Future<List<Newest>> fetchHottest() async {
+  final response = await http.get('https://lobste.rs/hottest.json');
+  var responseJson = json.decode(response.body);
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+//    return Newest.fromJson(json.decode(response.body));
+    return (responseJson as List).map((e) => Newest.fromJson(e)).toList();
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load newest stories');
+  }
+}
+
+class Newest {
+  String shortId;
+  String shortIdUrl;
+  String createdAt;
+  String title;
+  String url;
+  int score;
+  int upvotes;
+  int downvotes;
+  int commentCount;
+  String description;
+  String commentsUrl;
+  SubmitterUser submitterUser;
+  List<String> tags;
+
+  Newest(
+      {this.shortId,
+      this.shortIdUrl,
+      this.createdAt,
+      this.title,
+      this.url,
+      this.score,
+      this.upvotes,
+      this.downvotes,
+      this.commentCount,
+      this.description,
+      this.commentsUrl,
+      this.submitterUser,
+      this.tags});
+
+  Newest.fromJson(Map<String, dynamic> json) {
+    shortId = json['short_id'];
+    shortIdUrl = json['short_id_url'];
+    createdAt = json['created_at'];
+    title = json['title'];
+    url = json['url'];
+    score = json['score'];
+    upvotes = json['upvotes'];
+    downvotes = json['downvotes'];
+    commentCount = json['comment_count'];
+    description = json['description'];
+    commentsUrl = json['comments_url'];
+    submitterUser = json['submitter_user'] != null
+        ? new SubmitterUser.fromJson(json['submitter_user'])
+        : null;
+    tags = json['tags'].cast<String>();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['short_id'] = this.shortId;
+    data['short_id_url'] = this.shortIdUrl;
+    data['created_at'] = this.createdAt;
+    data['title'] = this.title;
+    data['url'] = this.url;
+    data['score'] = this.score;
+    data['upvotes'] = this.upvotes;
+    data['downvotes'] = this.downvotes;
+    data['comment_count'] = this.commentCount;
+    data['description'] = this.description;
+    data['comments_url'] = this.commentsUrl;
+    if (this.submitterUser != null) {
+      data['submitter_user'] = this.submitterUser.toJson();
+    }
+    data['tags'] = this.tags;
+    return data;
+  }
+}
+
+class SubmitterUser {
+  String username;
+  String createdAt;
+  bool isAdmin;
+  String about;
+  bool isModerator;
+  int karma;
+  String avatarUrl;
+  String invitedByUser;
+
+  SubmitterUser(
+      {this.username,
+      this.createdAt,
+      this.isAdmin,
+      this.about,
+      this.isModerator,
+      this.karma,
+      this.avatarUrl,
+      this.invitedByUser});
+
+  SubmitterUser.fromJson(Map<String, dynamic> json) {
+    username = json['username'];
+    createdAt = json['created_at'];
+    isAdmin = json['is_admin'];
+    about = json['about'];
+    isModerator = json['is_moderator'];
+    karma = json['karma'];
+    avatarUrl = json['avatar_url'];
+    invitedByUser = json['invited_by_user'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['username'] = this.username;
+    data['created_at'] = this.createdAt;
+    data['is_admin'] = this.isAdmin;
+    data['about'] = this.about;
+    data['is_moderator'] = this.isModerator;
+    data['karma'] = this.karma;
+    data['avatar_url'] = this.avatarUrl;
+    data['invited_by_user'] = this.invitedByUser;
+    return data;
+  }
+}
